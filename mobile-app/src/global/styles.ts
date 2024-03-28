f685c97b7665551a3caba4b33c228ed90fde8510
LuanMiranda77 <luanprof30@gmail.com>
2024-03-28 14:51:26 -0300

    Inicial api

---
+++ mobile-app/src/global/styles.ts
@@ -0,0 +1,13 @@
+export const theme={
+    colorPrimary:'#E8C547',
+    colorSecondary:'#30323d',
+    colorTertiary:'#4d5061',
+    colorBackground:'#f2f2f2',
+    colorBase:'#cdd1c4',
+    
+    // color notification
+    colorInfo:'#5c80bc',
+    colorError:'#A82826',
+    colorWarn:'#C36430',
+    colorSucess:'#259434'
+}
